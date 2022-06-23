407e3be26188ef8677e0a397f7d5e178ff8699f7
Jedidiah Foster <jedidiah.foster@gmail.com>
2022-06-23 13:06:35 -0700

    Skeletons of examples with bad values.

---
+++ examples/sns_subscription/main.tf
@@ -0,0 +1,7 @@
+# To be updated
+# module "sns_topic_subscription" {
+#   source    = "git::ssh://git@gilligan.pearsondev.com/tf-modules/sns.git//modules/sns_subscription?ref=<RELEASE_VERSION>"
+#   topic_arn = "<arn_of_sns_topic>"
+#   protocol  = "sqs"
+#   endpoint  = "<arn_sqs_queue>"
+# }
---
+++ examples/sns_topic_policy/main.tf
@@ -0,0 +1,7 @@
+# To be updated
+# module "sns_topic_policy" {
+#   source = "git::ssh://git@gilligan.pearsondev.com/tf-modules/sns.git//modules/sns_topic_policy?ref=<RELEASE_VERSION>"
+
+#   arn    = "<arn_of_topic>"
+#   policy = "<json_policy_to_attach>"
+# }
