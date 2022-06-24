87f44c16c82dadc07ba4ad0f8ce2385ac45f95ab
Jedidiah Foster <jedidiah.foster@gmail.com>
2022-06-24 12:56:39 -0700

    Testing pre-commit

--- locals.tf
+++ locals.tf
@@ -4,3 +4,5 @@ locals {
   name       = var.topic_name_override != null ? var.topic_name_override : var.topic_prefix == null ? format("%s", var.name) : format("%s-%s", var.name, var.topic_prefix)
   topic_name = var.fifo_topic ? format("%s.fifo", local.name) : local.name
 }
+
+
