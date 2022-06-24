66e708190053456357f3037e713b7658f225ef52
Jedidiah Foster <jedidiah.foster@gmail.com>
2022-06-24 09:20:24 -0700

    Testing tf-docs

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+/**
+ *
+ * Auto-generated technical documentation is created using [`terraform-docs`](https://terraform-docs.io/)
+ *
+ * # SNS Topic
+ *
+ * Creates an SNS Topic. Supports all standard options for the SNS Topic resource.
+ *
+ * Note that this module includes submodules for other common SNS needs, including:
+ *
+ * [sns_sms_preferences](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_sms_preferences)
+ *
+ * [sns_subscription](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_topic_subscription#protocol-support)
+ *
+ * [sns_topic_policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/)
+ *
+ * [sns_platform_application](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_platform_application)
+ *
+ * These submodules can be found under the [modules](modules) directory.
+ *
+ */
+
+
 terraform {
   required_version = ">= 0.14"
   required_providers {
--- modules/sns_platform_application/README.md
+++ modules/sns_platform_application/README.md
@@ -2,4 +2,4 @@
 
 Provides an SNS platform application resource.
 
-See the [Terraform documentation](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_platform_applicat) for more information on the limitations / requirements for using this resource.
+See the [Terraform documentation](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_platform_application) for more information on the limitations / requirements for using this resource.
